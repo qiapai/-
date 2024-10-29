3d615040ce725976011fe910f6235eb3a5efd11a
qiapai <135616382+qiapai@users.noreply.github.com>
2024-10-29 11:51:12 +0800

    Add files via upload

---
+++ owl.js
@@ -0,0 +1,11 @@
+$('.owl-carousel').owlCarousel({
+    singleItem: true,
+    loop: true,
+    margin: 0,
+    nav: true,
+    responsive: false,
+    items: 1,
+    navText: ["<img src='./res/arrow-l.png'>", "<img src='./res/arrow-r.png'>"],
+    autoplay: true,
+    autoplayTimeout: 5000,
+})
